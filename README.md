de7a50d1124dfe8b73a4c3a1b80d586ef7f97c02
Zhou-Shilin <2839997522@qq.com>
2023-07-29 08:41:52 +0800

    Create Repo

---
+++ README.md
@@ -0,0 +1,36 @@
+# UnionBanPlugin
+
+UnionBanPlugin 是一个 Bukkit 插件，使用 MySQL 数据库在多个服务器之间禁止玩家。
+
+## 安装
+
+1. 从 [GitHub Release](https://github.com/Minecraft-Public-Welfare-Alliance/UnionBan-Bukkit/releases) 下载最新版本的插件。
+2. 将下载的 JAR 文件放入 Bukkit 服务器的 `plugins` 文件夹。
+3. 重启启动服务器或使用PlugMan启用插件。
+4. 通过编辑位于 `plugins/MPWAUnionBan` 文件夹中的 `config.yml` 文件来配置插件。
+5. 在配置中输入数据库详细信息，将插件连接到您的 MySQL 数据库。
+6. 使用PlugMan重载插件
+
+## 使用方法
+
+- `/uban <playerID> <Reason(hacking, stealing, destroying, other)> <Reason_Text> <isOnline>` - 从联盟中禁止一个玩家。
+
+## 配置
+
+插件使用位于 `plugins/MPWAUnionBan` 文件夹中的 `config.yml` 文件进行配置。您可以修改以下设置：
+
+- `database`：数据库连接详细信息（主机、端口、数据库名称、用户名、密码）
+- `banCheckInterval`：刷新封禁列表的时间间隔（以秒为单位）
+- `from`：标识禁止发起的服务器
+
+## 贡献者
+
+- [BaimoQilin](https://github.com/Zhou-Shilin) (Zhou-Shilin, from [Hypiworld](https://baimoqilin.top/hypiworld))
+
+## TODO
+
+- [x] 支持使用`/uban`指令联合封禁玩家
+- [x] 支持在配置文件中配置服务器名称
+- [ ] 支持在腐竹试图将封禁玩家加入whitelist时给予提醒
+- [ ] 支持在配置文件中配置按类别封禁玩家
+- [ ] 支持导入banlist
---
+++ src/main/java/top/baimoqilin/mpwaunionban/Main.java
@@ -0,0 +1,175 @@
+package top.baimoqilin.mpwaunionban
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.sql.*;
+
+public class UnionBanPlugin extends JavaPlugin {
+    private Connection connection;
+    private String host, port, database, username, password, fromServer;
+    private int banCheckInterval;
+
+    @Override
+    public void onEnable() {
+        // Load configuration
+        loadConfiguration();
+
+        // Connect to MySQL database
+        connectToDatabase();
+
+        // Schedule ban check task
+        getServer().getScheduler().runTaskTimerAsynchronously(this, this::checkForBannedPlayers, 0L, banCheckInterval);
+
+        getCommand("uban").setExecutor(this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Disconnect from the database
+        disconnectFromDatabase();
+    }
+
+    private void loadConfiguration() {
+        saveDefaultConfig();
+        getConfig().options().copyDefaults(true);
+
+        // Read configuration values
+        host = getConfig().getString("database.host");
+        port = getConfig().getString("database.port");
+        database = getConfig().getString("database.database");
+        username = getConfig().getString("database.username");
+        password = getConfig().getString("database.password");
+        banCheckInterval = getConfig().getInt("banCheckInterval") * 20; // Convert to ticks (20 ticks = 1 second)
+        fromServer = getConfig().getInt("from");
+    }
+
+    private void connectToDatabase() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" +
+                    database + "?useSSL=false", username, password);
+            getLogger().info("Connected to the MySQL database.");
+        } catch (SQLException e) {
+            getLogger().severe("Failed to connect to the MySQL database: " + e.getMessage());
+        }
+    }
+
+    private void disconnectFromDatabase() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+                getLogger().info("Disconnected from the MySQL database.");
+            }
+        } catch (SQLException e) {
+            getLogger().severe("Failed to disconnect from the MySQL database: " + e.getMessage());
+        }
+    }
+
+    private int getRows() {
+        try (PreparedStatement statement = connection.prepareStatement("SELECT COUNT(*) FROM UnionBan")) {
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return resultSet.getInt(1);
+            }
+        } catch (SQLException e) {
+            getLogger().severe("Error retrieving row count: " + e.getMessage());
+        }
+        return 0;
+    }
+
+    private void addRow(String ID, String IP, int Reason, String Reason_Text, boolean isOnline, String From) {
+    private void addRow(String ID, String IP, int Reason, String Reason_Text, boolean isOnline, String From) {
+        try (PreparedStatement statement = connection.prepareStatement(
+                "INSERT INTO UnionBan (ID, IP, Reason, Reason_Text, isOnline, From) VALUES (?, ?, ?, ?, ?, ?)")) {
+            statement.setString(1, ID);
+            statement.setString(2, IP);
+            statement.setInt(3, Reason);
+            statement.setString(4, Reason_Text);
+            statement.setBoolean(5, isOnline);
+            statement.setString(6, From);
+            statement.executeUpdate();
+            getLogger().info("Added a new row to the UnionBan table.");
+        } catch (SQLException e) {
+            getLogger().severe("Error adding a row to the UnionBan table: " + e.getMessage());
+        }
+    }
+
+    private void checkForBannedPlayers() {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            if (!isPlayerBanned(player)) {
+                player.kickPlayer("You are banned."); // Modify as needed
+            }
+        }
+    }
+
+    private boolean isPlayerBanned(Player player) {
+        try (PreparedStatement statement = connection.prepareStatement(
+                "SELECT * FROM UnionBan WHERE ID=? OR IP=?")) {
+            statement.setString(1, player.getUniqueId().toString());
+            statement.setString(2, player.getAddress().getAddress().getHostAddress());
+            ResultSet resultSet = statement.executeQuery();
+            return resultSet.next();
+        } catch (SQLException e) {
+            getLogger().severe("Error checking ban status for player " + player.getName() + ": " + e.getMessage());
+        }
+        return false;
+    }
+
+    private int getReason(String reason) {
+        switch (reason.toLowerCase()) {
+            case "hacking":
+                return 0;
+            case "stealing":
+                return 1;
+            case "destroying":
+                return 2;
+            case "other":
+                return 3;
+            default:
+                return -1;
+        }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("uban")) {
+            if (!sender.hasPermission("unionban.uban")) {
+                sender.sendMessage("You don't have permission to use this command.");
+                return true;
+            }
+
+            if (args.length < 4) {
+                sender.sendMessage("Usage: /uban <playerID> <Reason(hacking, stealing, destroying, other)> <Reason_Text> <isOnline>");
+                return true;
+            }
+
+            String playerID = args[0];
+            int reason = getReason(args[1]);
+            if (reason == -1) {
+                sender.sendMessage("Invalid reason. Available reasons: hacking, stealing, destroying, other.");
+                return true;
+            }
+            String reasonText = args[2];
+            boolean isOnline = Boolean.parseBoolean(args[3]);
+
+            Player target = Bukkit.getPlayer(playerID);
+            if (target != null && target.isOnline()) {
+                // The target player is online
+                addRow(target.getUniqueId().toString(), target.getAddress().getAddress().getHostAddress(),
+                        reason, reasonText, isOnline, fromServer);
+                sender.sendMessage("Successfully added a new row to the UnionBan table.");
+            } else {
+                // The target player is not online
+                addRow(playerID, "UNKNOWN", reason, reasonText, isOnline, fromServer);
+                sender.sendMessage("Successfully added a new row to the UnionBan table.");
+            }
+
+            return true;
+        }
+        return false;
+    }
+}
